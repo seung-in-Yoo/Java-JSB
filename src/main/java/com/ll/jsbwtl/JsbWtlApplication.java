d30a6eb4de5210efd2f44ee9e0b520a8663e7966
seungin <tmddls91@inha.edu>
2025-08-25 18:45:17 +0900

    [init] 프로젝트 초기 세팅

---
+++ src/main/java/com/ll/jsbwtl/JsbWtlApplication.java
@@ -0,0 +1,13 @@
+package com.ll.jsbwtl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsbWtlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsbWtlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/jsbwtl/JsbWtlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.jsbwtl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsbWtlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
