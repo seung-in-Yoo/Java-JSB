775bf9d7a06cb1fa8ad20440c1b270e1a4fea811
seungin <tmddls91@inha.edu>
2025-08-27 18:53:35 +0900

    [feat] 정적 리소스, 타임리프 페이지 경로 관련 설정을 위한 WebMvcConfig 구현

--- src/main/java/com/ll/jsbwtl/config/WebMvcConfig.java
+++ src/main/java/com/ll/jsbwtl/config/WebMvcConfig.java
@@ -1,16 +1,32 @@
 package com.ll.jsbwtl.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    // 정적 리소스 경로 설정
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+    }
+
+    // CORS 설정
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("*");
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+    // 로그인 성공 시나 기본 접속 URL을 list.html로 리디렉션
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/")
+                .setViewName("redirect:/question/list");
     }
 }
