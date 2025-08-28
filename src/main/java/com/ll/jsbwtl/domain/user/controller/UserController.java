24ce93d8fad1e57b6bd4fd8cf3b99dc588c710d1
Jeongho Kim <kimjungho.kr@gmail.com>
2025-08-28 17:42:23 +0900

    [Feat] 소셜로그인 인증기능 구현

--- src/main/java/com/ll/jsbwtl/domain/user/controller/UserController.java
+++ src/main/java/com/ll/jsbwtl/domain/user/controller/UserController.java
@@ -2,8 +2,14 @@ package com.ll.jsbwtl.domain.user.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 // 예시 코드입니다. (지우시고 자유롭게 개발하셔도 돼요)
 @Controller
 public class UserController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "user/login"; // templates/login.html 로 이동
+    }
 }
--- src/main/java/com/ll/jsbwtl/domain/user/service/UserService.java
+++ src/main/java/com/ll/jsbwtl/domain/user/service/UserService.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Service;
 // 예시 코드입니다. (지우시고 자유롭게 개발하셔도 돼요)
 @Service
 public class UserService {
+
+
 }
