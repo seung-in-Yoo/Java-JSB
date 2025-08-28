3239e584d2dd4dae35f424be3669837442c73c84
seungin <tmddls91@inha.edu>
2025-08-28 10:12:01 +0900

    [fix] CORS 설정 로직 수정

--- src/main/java/com/ll/jsbwtl/config/WebMvcConfig.java
+++ src/main/java/com/ll/jsbwtl/config/WebMvcConfig.java
@@ -17,7 +17,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
