fc03b4f4cc3348c541228a0e1d5cea519bcc751e
TooTo <wlguq900@gmail.com>
2025-09-01 02:45:51 +0900

    [Feat] dto ì¶생성

---
+++ src/main/java/com/ll/jsbwtl/domain/user/dto/UserSignupResponse.java
@@ -0,0 +1,10 @@
+package com.ll.jsbwtl.domain.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserSignupResponse {
+    private String message;
+}
