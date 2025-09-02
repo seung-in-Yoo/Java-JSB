6ef67fcfd8416b23c8139addd3cca2ac644fc3ec
Jeongho Kim <kimjungho.kr@gmail.com>
2025-09-02 16:16:00 +0900

    [Feat] 소셜 로그인시 발행된 jwt 로컬스토리지에 저장 구현

--- src/main/java/com/ll/jsbwtl/domain/user/controller/UserController.java
+++ src/main/java/com/ll/jsbwtl/domain/user/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.ll.jsbwtl.domain.user.controller;
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 // 예시 코드입니다. (지우시고 자유롭게 개발하셔도 돼요)
 @Controller
@@ -11,5 +17,12 @@ public class UserController {
     @GetMapping("/login")
     public String loginPage() {
         return "user/login"; // templates/login.html 로 이동
+    }
+
+    @GetMapping("/login/success")
+    public String successPage(HttpServletRequest request, Model model) {
+        String accessToken = (String) request.getSession().getAttribute("accessToken");
+        model.addAttribute("accessToken", accessToken);
+        return "user/login-success"; // user/login-success.html
     }
 }
